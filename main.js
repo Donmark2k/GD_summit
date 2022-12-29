9c2eda69f60bf8162dbc5fdf678570d308134834
Donmark2k <chukwuemekavp@gmail.com>
2022-12-29 08:13:56 +0100

    design the about.html page

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
-// const navLogo = document.querySelector('.nav-branding');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
-//   navLogo.classList.toggle('active');
 });
 
 document.querySelectorAll('.nav-link').forEach((n) => n
